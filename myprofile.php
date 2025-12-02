43657d785701d638417190e3008d6711fdfb67a3
Ahmad Raza <arjbrand56@gmail.com>
2025-12-02 00:33:17 -0700

    Add files via upload
    My php project.

---
+++ myprofile.php
@@ -0,0 +1,58 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>Profile</title>
+</head>
+<body class="bg-black">
+    <div class="conatiner-fluid my-5">
+        <div class="container w-50 bg-dark p-3">
+            <div class="row ">
+                <div class="col-md-4 ">
+                    <img src="./images/Ahmad.jpg" class="img-fluid" style="width: 9rem; height: 9rem;"  alt="">
+                </div>
+                <div class="col-md-8" >
+                    <h2 class="text-white ">Admin Id : ba56</h2>
+                    <h4 class="text-secondary">Admin Name : Ahmad Raza</h4>
+                    <p class="text-secondary fs-6">Email : arjbrand56@gmail.com</p>
+                    <span class="text-secondary">Platform: </span>
+                    <span class="badge bg-primary">HTML</span>
+                    <span class="badge bg-info">CSS</span>
+                    <span class="badge bg-warning">Bootstrap</span>
+                    <span class="badge bg-success">JavaScript</span>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-4 text-center">
+                    <h1 class="text-secondary">5.2K</h1>
+                    <p class="text-secondary">Fans</p>
+                    <button class="btn btn-primary px-5">
+                        <i class="fa-brands fa-facebook-f fa-xs" style="color: #ffffff; padding-right: 1px;"></i>Like</button>
+                </div>
+                <div class="col-md-4 text-center">
+                    <h1 class="text-secondary">1.4K</h1>
+                    <p class="text-secondary">Following</p>
+                    <button class="btn btn-success px-5">
+                        <i class="fa-brands fa-x-twitter fa-xs" style="color: #ffffff; "></i>Tweet</button>
+                </div>
+                <div class="col-md-4 text-center">
+                    <h1 class="text-secondary">3.8K</h1>
+                    <p class="text-secondary">Followers</p>
+
+                    <button class="btn btn-warning px-4 text-white"><i class="fa-brands fa-google-plus-g" style="color: #f7f7f7;"></i>
+                        Follow</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
